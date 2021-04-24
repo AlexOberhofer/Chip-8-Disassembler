23375d5e2baedef38002a37f866b3784fbec8b59
Alex Oberhofer <alexmoberhofer@gmail.com>
2021-04-23 23:30:08 -0400

    emscripten experimenting

--- src/chip8.c
+++ src/chip8.c
@@ -1,4 +1,74 @@
-#include "chip8.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <SDL2/SDL.h>
+
+#define MEM_SIZE 4096
+#define NUM_REGISTERS 16
+#define SCREEN_W 640
+#define SCREEN_H 320
+#define SCREEN_BPP 32
+#define W 64
+#define H 32
+
+typedef struct C8 {
+
+  uint8_t V[NUM_REGISTERS];
+  uint16_t stack[NUM_REGISTERS];
+  short I;
+  uint8_t sp;
+  uint16_t pc;
+  uint8_t delay;
+  uint8_t timer;
+  unsigned char *memory;
+  unsigned char screen[64*32]; //starts at 0xF00
+
+} C8;
+
+typedef struct C8_display {
+    SDL_Window *window;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+} C8_display;
+
+unsigned char fonts[80] =
+{
+  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+  0x20, 0x60, 0x20, 0x20, 0x70,
+  0xF0, 0x10, 0xF0, 0x80, 0xF0,
+  0xF0, 0x10, 0xF0, 0x10, 0xF0,
+  0x90, 0x90, 0xF0, 0x10, 0x10,
+  0xF0, 0x80, 0xF0, 0x10, 0xF0,
+  0xF0, 0x80, 0xF0, 0x90, 0xF0,
+  0xF0, 0x10, 0x20, 0x40, 0x40,
+  0xF0, 0x90, 0xF0, 0x90, 0xF0,
+  0xF0, 0x90, 0xF0, 0x10, 0xF0,
+  0xF0, 0x90, 0xF0, 0x90, 0x90,
+  0xE0, 0x90, 0xE0, 0x90, 0xE0,
+  0xF0, 0x80, 0x80, 0x80, 0xF0,
+  0xE0, 0x90, 0x90, 0x90, 0xE0,
+  0xF0, 0x80, 0xF0, 0x80, 0xF0,
+  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
+static int key_map[0x10] =  {
+  SDL_SCANCODE_X, SDL_SCANCODE_1, SDL_SCANCODE_2, SDL_SCANCODE_3,
+  SDL_SCANCODE_Q, SDL_SCANCODE_W, SDL_SCANCODE_E, SDL_SCANCODE_A,
+  SDL_SCANCODE_S, SDL_SCANCODE_D, SDL_SCANCODE_Z, SDL_SCANCODE_C,
+  SDL_SCANCODE_4, SDL_SCANCODE_R, SDL_SCANCODE_F, SDL_SCANCODE_V
+};
+
+void instructionNotImplemented(uint16_t opcode, uint16_t pc);
+void disassembleChip8Op(uint8_t *codebuffer, int pc);
+int process_keypress(SDL_Event *e);
+void process_timers(C8 *c);
+void executeOp(C8* c);
+void init(FILE *f, C8 * c);
+void sdl_draw(C8 *c, C8_display *display);
+void dumpMem(C8 * c);
+void dumpReg(C8 * c);
+
 
 void executeOp(C8* c) {
   uint8_t *code = &c->memory[c->pc];
@@ -476,22 +546,13 @@ int main(int argc, char* argv[]) {
   C8 * c = calloc(sizeof(C8), 1);
   C8_display *display = malloc(sizeof(C8_display));
 
-  //OPEN FILE
-  if(argc == 2){
-      f = fopen(argv[1], "rb");
-  } else if (argc == 3) {
-      f = fopen(argv[2], "rb");
-      if(strcmp(argv[1], "--debug") == 0){
-        debug_flag = 1;
-        printf("debug_flag set.");
-      } else {
-        printf("Flags include --debug.\n");
-        exit(1);
-      }
-  }
+  const char* ROM = "./maze.ch8";
+  f = fopen(ROM, "rb");
+
+  printf("File read");
 
   if (f == NULL) {
-    printf("Error: Could not open %s\n", argv[1]);
+    printf("Error: Could not open %s\n", ROM);
     printf("Usage: chip8 <flag> <path/to/rom>");
     exit(1);
   }
@@ -499,7 +560,7 @@ int main(int argc, char* argv[]) {
   //initialize memory values
   init(f, c);
 
-  if(SDL_Init(SDL_INIT_EVERYTHING) < 0){
+  if(SDL_Init(SDL_INIT_VIDEO) < 0){
       printf("SDL_Init failed: %s\n", SDL_GetError());
       exit(1);
   } else {
---
+++ src/emscripten.sh
@@ -0,0 +1,1 @@
+emcc -s USE_SDL=2 -s USE_SDL_IMAGE=2 --preload-file ./maze.ch8 chip8.c -o chip8.html
